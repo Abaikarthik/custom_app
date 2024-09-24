d927875eea6d697bcdac905fe88a1f4872e379af
Abaikarthik <144216762+Abaikarthik@users.noreply.github.com>
2024-09-24 17:45:37 +0530

    Add files via upload

---
+++ custom_app/custom_app/doctype/label_details_table/label_details_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Abaikarthik and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LabelDetailsTable(Document):
+	pass
